465edbeb7c0cb78401eb1f7e535277fb3572514a
archergs <archergsdev@gmail.com>
2020-02-03 20:18:14 +1030

    added google auth

--- daymap2calendar.py
+++ daymap2calendar.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import requests
 import pandas as pd
 from requests_ntlm import HttpNtlmAuth
@@ -5,6 +6,11 @@ from bs4 import BeautifulSoup
 import re
 import math
 import time
+import os
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 # parse timetable into list
 def parseTimetable(timetable):
@@ -33,10 +39,67 @@ def parseTimetable(timetable):
     return sorted_data
 
 def googleAuth():
-    print("google auth")
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('calendar', 'v3', credentials=creds)
+
+def publishClassesToGoogle(service):
+    print("Sending classes to Google Calendar. Please wait...")
+
 
 validLines = [1,3,4,5,7,8,9,12,13,15] # only these rows in daymap's timetable contain lessons
-sorted_data =[]
+sorted_data = []
+
+# google api scope. r/w access to events is all thats needed
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+# google event JSON template
+event = {
+  'summary': '',
+  'location': '',
+  'description': '',
+  'start': {
+    'dateTime': '',
+    'timeZone': '',
+  },
+  'end': {
+    'dateTime': '2015-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+  ],
+  'reminders': {
+    'useDefault': False,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+}
 
 #username and password
 username = input(r"Enter Daymap username (e.g. domain\firstname.lastname): ")
@@ -53,7 +116,8 @@ assessment = session.get("http://daymap.gihs.sa.edu.au/student/assessmentsummary
 if timetable.status_code == 200 and assessment.status_code == 200:
     print("status 200 OK, proceeding to parse ASPX...")
     parsedTimetable = parseTimetable(timetable.text)
-    googleAuth()
+    googleService = googleAuth()
+    publishClassesToGoogle(googleService)
 
 else:
     print(f"Login failed, try again later. Quitting... (error code: {timetable.status_code} {assessment.status_code})")
